2a782a6ecf64614f66f28ba2ece9cab8430bbc09
iammuttaqi <muntasermuttaqi@gmail.com>
2024-10-22 09:54:49 +0600

    home redirect to login

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,8 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('filament.user.auth.login');
+    // return view('welcome');
 });
 
 Route::get('sl/{user_id}', function ($user_id) {
